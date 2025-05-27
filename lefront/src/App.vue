e7032771cc45a191e1a15da4db25fb21db83ea70
Marcferni <marcferni@hotmail.com>
2025-05-27 15:21:40 +0200

    Initial commit: Creada estructura del proyecto con backend Django (myend) y frontend (Vite + Vue + Tailwind CSS)

---
+++ lefront/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class='bg-blue-500 text-white p-4'>
+    ¡Hola! Tailwind CSS y Vue están funcionando.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style scoped>
+/* Puedes agregar estilos adicionales aquí */
+</style>
