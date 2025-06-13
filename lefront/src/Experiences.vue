a0c60a210dd4a2ab47c0697a5b5969843a58bee1
Marcferni <marcferni@hotmail.com>
2025-06-13 17:43:46 +0200

    feat: integrar iframe de Django en Vue via Experiences.vue

---
+++ lefront/src/Experiences.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="max-w-4xl mx-auto p-6">
+    <h2 class="text-2xl font-bold mb-4">Experiencias Curadas</h2>
+    <iframe
+      src="http://127.0.0.1:8000/experiences/"
+      class="w-full h-[600px] border-0 rounded-lg shadow"
+      title="Lista de Experiencias"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Experiences",
+};
+</script>
+
+<style scoped>
+/* si quieres ajustar la altura en móvil, por ejemplo */
+@media (max-width: 640px) {
+  iframe {
+    height: 400px;
+  }
+}
+</style>
