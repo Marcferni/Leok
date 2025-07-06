11d82f9a80adaee2e0e1e36c9010e98859cd58e8
Marcferni <marcferni@hotmail.com>
2025-07-06 17:18:16 +0200

    Delete leback directory
    Manual solution from local problem

--- leback/experiences/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Experience
-
-def experience_list(request):
-    experiences = Experience.objects.all().order_by('-created_at')
-    return render(request, "list.html", {"experiences": experiences})
